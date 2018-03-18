008a3bf35c94318b72620210c58e0d0b587380f5
Jacek Schwalbe Staszewski <schwalbe1728@gmail.com>
2018-03-18 23:48:25 +0100

    Created item templates and programmed custom inspector for them

---
+++ Assets/Editor/Custom Inspectors/ItemCustomInspector.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(ItemScript))]
+public class ItemCustomInspector : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()    
+    {        
+        ItemScript selected = target as ItemScript;
+
+        bool save = false;
+
+        save |= SerializedStringProperty("name", "Name");
+        save |= SerializedIntProperty("value", "Abstract Value", true);
+        save |= SerializedEquipmentType("Equipment Type");
+
+        if(selected.IsWeapon)
+        {
+            save |= DrawPropertiesOfWeapon();
+        }
+
+        if(selected.IsEdible)
+        {
+            save |= DrawPropertiesOfEdible();
+        }
+
+        if (save)
+        {
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        EditorGUILayout.LabelField("Monetary value", selected.MonetaryValueInCoin());
+    }    
+
+    private bool SerializedStringProperty(string name, string label)
+    {
+        bool result = false;
+        string oldString = serializedObject.FindProperty(name).stringValue;
+        string newString = EditorGUILayout.TextField(label, oldString);
+
+        if (!newString.Equals(oldString))
+        {
+            serializedObject.FindProperty(name).stringValue = newString;
+            result = true;
+        }
+
+        return result;
+    }
+
+    private bool SerializedIntProperty(string name, string label, bool positiveOrZero, string notSmallerThanProperty = "")
+    {
+        int notSmallerThan =
+            (notSmallerThanProperty != null && !notSmallerThanProperty.Equals("")) ?
+                serializedObject.FindProperty(notSmallerThanProperty).intValue :
+                (positiveOrZero ? 0 : int.MinValue);
+
+        bool result = false;
+        int oldInt = serializedObject.FindProperty(name).intValue;
+        int newInt = EditorGUILayout.IntField(label, oldInt);
+
+        if (newInt < notSmallerThan) newInt = notSmallerThan;
+
+        if (newInt != oldInt)
+        {
+            serializedObject.FindProperty(name).intValue = newInt;
+            result = true;
+        }
+
+        return result;
+    }
+
+    private bool SerializedEquipmentType(string label)
+    {
+        bool result = false;
+
+        EquipmentType oldType = (EquipmentType)serializedObject.FindProperty("type").enumValueIndex;
+        EquipmentType newType = (EquipmentType)EditorGUILayout.EnumPopup(label, oldType);
+
+        if(oldType != newType)
+        {
+            serializedObject.FindProperty("type").enumValueIndex = (int)newType;
+            result = true;
+        }
+
+        return result;
+    }
+
+    private bool DrawPropertiesOfWeapon()
+    {
+        bool result = false;
+
+        result |= SerializedIntProperty("bonusDamageMin", "Min Damage", true);
+        result |= SerializedIntProperty("bonusDamageMax", "Max Damage", true, "bonusDamageMin");
+
+        return result;
+    }
+
+    private bool DrawPropertiesOfEdible()
+    {
+        bool result = false;
+
+        result |= SerializedIntProperty("timeConsumed", "Consumption Time", true);
+        result |= SerializedIntProperty("vitalityHealed", "Vitality Heal", false);
+        result |= SerializedIntProperty("sanityHealed", "Sanity Heal", false);
+
+
+        return result;
+    }
+}
---
+++ Assets/Prefabs/Items/ItemScript.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Equipment Item")]
+public class ItemScript : ScriptableObject
+{
+    [SerializeField]
+    private new string name;
+    [SerializeField]    
+    private int value;
+    [SerializeField]
+    private EquipmentType type;
+
+    public string Name { get { return name; } }
+    public int AbstractValue { get { return value; } }
+    public EquipmentType EqType { get { return type; } }
+
+    #region Weapon Specific
+    public bool IsWeapon
+    {
+        get
+        {
+            return EqType == EquipmentType.OneHandedWeapon || EqType == EquipmentType.TwoHandedWeapon;
+        }
+    }
+
+    [SerializeField]
+    private int bonusDamageMin;
+
+    [SerializeField]
+    private int bonusDamageMax;
+
+    public int BonusDamage
+    {
+        get { return IsWeapon ? Random.Range(bonusDamageMin, bonusDamageMax + 1) : 0; }
+    }
+
+    #endregion
+
+    #region Edibles Specific
+    public bool IsEdible { get { return EqType == EquipmentType.Consumables; } }
+
+    [SerializeField]
+    private int timeConsumed;
+
+    [SerializeField]
+    private int vitalityHealed;
+
+    [SerializeField]
+    private int sanityHealed;
+    #endregion
+}
+
+public enum EquipmentType
+{
+    OneHandedWeapon,
+    TwoHandedWeapon,
+    Clothes,
+    Consumables,
+    Money,
+    Quest
+}
+
+public static class ItemExtension
+{
+    private const int AbstractToPence = 4;
+    private const int PenceToShilling = 12;
+    private const int ShillingToPound = 20;    
+
+    public static string MonetaryValueInCoin(this ItemScript item, int quantity = 1)
+    {
+        int totalValue = item.AbstractValue * quantity;
+
+        int pounds, shillings;
+        float pences;
+
+        ToCoins(totalValue, out pounds, out shillings, out pences);
+
+        StringBuilder sb = new StringBuilder();
+
+        if(pounds > 0)
+        {
+            sb.Append(pounds);
+            sb.Append(" Pounds");
+        }
+
+        if(shillings > 0)
+        {
+            if(sb.Length > 0)
+            {
+                sb.Append(", ");
+            }
+
+            sb.Append(shillings);
+            sb.Append(" Shillings");
+        }
+
+        if (pences > 0)
+        {
+            if (sb.Length > 0)
+            {
+                sb.Append(", ");
+            }
+
+            sb.AppendFormat("{0:0.00} Pences", pences);
+        }
+
+        if(totalValue == 0)
+        {
+            sb.Append("N/A");
+        }
+
+        return sb.ToString();
+    }
+
+    public static void GetValuesInCoin(this ItemScript item, out int pounds, out int shillings, out float pences)
+    {
+        int value = item.AbstractValue;
+
+        ToCoins(value, out pounds, out shillings, out pences);
+    }
+
+    public static void GetValuesInCoin(this ItemScript[] items, out int pounds, out int shillings, out float pences)
+    {
+        int value = 0;
+        foreach(ItemScript item in items) { value += item.AbstractValue; }
+
+        ToCoins(value, out pounds, out shillings, out pences);
+    }
+
+    public static float ValueToPence(this ItemScript item)
+    {        
+        return ToPence(item.AbstractValue);
+    }
+
+    public static int ValueToShilling(this ItemScript item)
+    {
+        return ToShilling(item.AbstractValue);
+    }
+
+    public static int ValueToPound(this ItemScript item)
+    {
+        return ToPound(item.AbstractValue);
+    }
+
+    public static void ToCoins(int value, out int pounds, out int shillings, out float pences)
+    {
+        pounds = ToPound(value);
+        value -= pounds * ShillingToPound * PenceToShilling * AbstractToPence;
+
+        shillings = ToShilling(value);
+        value -= shillings * PenceToShilling * AbstractToPence;
+
+        pences = ToPence(value);
+    }
+
+    private static float ToPence(float val)
+    {
+        return (val / AbstractToPence);
+    }
+
+    private static int ToShilling(float val)
+    {
+        return Mathf.FloorToInt(val / (AbstractToPence * PenceToShilling));
+    }
+
+    private static int ToPound(float val)
+    {
+        return Mathf.FloorToInt(val / (AbstractToPence * PenceToShilling * ShillingToPound));
+    }    
+}
